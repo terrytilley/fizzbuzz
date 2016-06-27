067f8c1d06af6717ada1ce5cc78a4c3c9ed84c68
Terry <terry@terrytilley.com>
2016-06-27 15:29:18 +0100

    Refactored fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,14 +1,11 @@
 def fizzbuzz(number="", *arguments)
-  if arguments == []
-    if number.is_a? Integer
-      return 'fizz'     if (number % 3 == 0) && (number % 5 != 0)
-      return 'buzz'     if (number % 5 == 0) && (number % 3 != 0)
-      return 'fizzbuzz' if (number % 3 == 0) && (number % 5 == 0)
-      number
-    else
-      return "Not a valid number"
-    end
+  return "Please only input one argument" if arguments.length >= 1
+  if number.is_a? Integer
+    return 'fizz'     if (number % 3 == 0) && (number % 5 != 0)
+    return 'buzz'     if (number % 5 == 0) && (number % 3 != 0)
+    return 'fizzbuzz' if (number % 3 == 0) && (number % 5 == 0)
+    number
   else
-    return "Please only input one argument"
+    return "Not a valid number"
   end
 end
